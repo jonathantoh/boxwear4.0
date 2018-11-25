1523f7829ad2e96210263b46ad18fd35fcc15da9
Jonathan <jonathantohjj@gmail.com>
2018-11-26 01:24:12 +0800

    !NEW JudoVan deliverysite
    V0.1 deliverysite(supplier) push
    email: supportjudo@gmail.com
    pass: 123

---
+++ DeliverySite/delivery-login.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DeliverySite_login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["deliveryadmin"] != null)
+        {
+            Response.Redirect("~/DeliverySite/delivery-dashboard.aspx");
+        }
+    }
+
+    protected void btn_submit_Click(object sender, EventArgs e)
+    {
+        bool result;
+        if(txtusername.Text=="supportjudo@gmail.com" && txtpassword.Text == "123")
+        {
+            result = true;
+        }else
+        {
+            result = false;
+        }
+
+        if (result == true)
+        {
+            Session["deliveryadmin"] = txtusername.Text;
+            Response.Redirect("~/DeliverySite/delivery-dashboard.aspx");
+        }
+        else
+        {
+            lblerror.Text = "Incorrect details";
+        }
+    }
+}
